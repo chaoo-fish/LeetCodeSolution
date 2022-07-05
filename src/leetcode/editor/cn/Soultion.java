5d393e1c6dcb00dfc2bc84c3c3b96a2bd5f9e892
xiaozhao <chao2286@163.com>
2022-07-05 10:43:29 +0800

    teest

--- src/leetcode/editor/cn/Soultion.java
+++ src/leetcode/editor/cn/Soultion.java
@@ -1,5 +1,7 @@
 package leetcode.editor.cn;
 
+import java.util.*;
+
 /**
  * 通用模板
  */
@@ -7,20 +9,9 @@ public class Soultion {
     public static void main(String[] args) {
     }
 
-    public int longestCommonSubsequence(String text1, String text2) {
-        int M = text1.length() + 1;
-        int N = text2.length() + 1;
-        int[][] dp = new int[M + 1][N + 1];
-        for (int i = 1; i <= M; i++) {
-            for (int j = 1; j <= N; j++) {
-                if (text1.charAt(i - 1) == text2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1] + 1;
-                } else {
-                    dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]);
-                }
-            }
-        }
-        return dp[M][N];
+    public List<List<Integer>> minimumAbsDifference(int[] arr) {
+        // 先排序 默认牲畜
+        Arrays.sort(arr);
+        return null;
     }
-
 }
